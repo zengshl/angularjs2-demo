ed5842f07581a49c98c4914a98f455ff46cc10c2
wss <wss@163.com>
2016-06-16 14:09:32 +0800

    FEATURE 个人信息重新布局，表单验证 BUGFIX  表单未验证时，提交按钮不可点的bug修改

--- src/app/+personalset/components/personalset.ts
+++ src/app/+personalset/components/personalset.ts
@@ -7,39 +7,42 @@ import {AfterViewInit} from "@angular/core";
 import  {User,UserCompany} from '../../shared/index';
 import {UtilService} from "../../shared/index";
 import {Response} from '@angular/http';
-//验证组件加载
+////验证组件加载
 import {FORM_DIRECTIVES} from '@angular/common';
 import { Control, ControlGroup } from '@angular/common';
 import { ValidationMessagesComponent } from 'ng2-validate/core';
 import {EmailValidation, MobileValidation, PasswordValidation, PersonIdValidation} from "../../shared/index";
 
+
 declare var jQuery:JQueryStatic;
 
 @Component({
-  selector: 'navbar-box',
+  selector: 'personal-set',
   providers:[UtilService],
   directives: [FORM_DIRECTIVES,ValidationMessagesComponent],
   template: require('app/+personalset/components/personalset.html')
 })
 export class PersonalSetComponent implements AfterViewInit{
   private myForm: ControlGroup;
+  private emailControl: Control;
   private mobileControl: Control;
-  //private emailControl: Control;
+
+  private isValid:boolean;
 
   user:User;
   company:UserCompany;
+  showbase:boolean = true;
+  showvalidate:boolean = false;
+  showcompany:boolean = false;
+  //步骤
+  step1:string = 'active';
+  step2:string = '';
+  step3:string = '';
 
-  ngAfterViewInit() {
-    //jQuery('#leftMenu')
-    //  .accordion();
-    this.mobileControl = new Control('', new MobileValidation().validator);
-    //this.emailControl = new Control('', new EmailValidation().validator);
-    this.myForm = new ControlGroup({
+  AfterViewInit(){
 
-      //emailControl: this.emailControl,
-      mobileControl : this.mobileControl
-    });
   }
+
   constructor(private router:Router,private _util:UtilService)  {
     this.user = new User();
     this.company = new UserCompany();
@@ -47,9 +50,17 @@ export class PersonalSetComponent implements AfterViewInit{
       this.user = JSON.parse(sessionStorage.getItem('user'))
       this.freshUser();
     }
+    this.emailControl = new Control('', new EmailValidation().validator);
+    this.mobileControl = new Control('', new MobileValidation().validator);
+    //this.passwordControl = new Control('', new PasswordValidation().validator);
+    //this.personidControl = new Control('', new PersonIdValidation().validator);
+    this.myForm = new ControlGroup({
+      emailControl: this.emailControl,
+      mobileControl : this.mobileControl
+      //passwordControl: this.passwordControl,
+      //personidControl : this.personidControl
+    });
   }
-
-
 
   nav(name:string){
     this.router.parent.navigate([name]);
@@ -59,18 +70,22 @@ export class PersonalSetComponent implements AfterViewInit{
   }
 
   choose(value:string){
-    console.log(value);
+      console.log('value');
   }
 
   updataUser(){
-    this._util.updataUser(JSON.stringify(this.user)).subscribe((res:Response)=>{
-      this.freshUser();
-      if(res.json() !=0){
-        alert("更新成功");
-      }else{
-        alert("更新失败");
-      }
-    });
+    if(this.myForm.valid){
+      this._util.updataUser(JSON.stringify(this.user)).subscribe((res:Response)=>{
+        this.freshUser();
+        if(res.json() !=0){
+          alert("更新成功");
+        }else{
+          alert("更新失败");
+        }
+      });
+    }else{
+      console.log('请完善必填信息');
+    }
   }
 
   //刷新数据
@@ -81,5 +96,32 @@ export class PersonalSetComponent implements AfterViewInit{
         this.company = resp.json().company
       }
     });
+  }
+
+  stepClick(step:string){
+    console.log(step);
+    //event.target.style.background = 'LightBLue';
+    if(step == 'step1'){
+      this.step1 = "active";
+      this.step2 = "";
+      this.step3 = "";
+      this.showbase = true;
+      this.showvalidate = false;
+      this.showcompany = false;
+    }else if(step == 'step2'){
+      this.step1 = "";
+      this.step2 = "active";
+      this.step3 = "";
+      this.showbase = false;
+      this.showvalidate = true;
+      this.showcompany = false;
+    }else{
+      this.step1 = "";
+      this.step2 = "";
+      this.step3 = "active";
+      this.showbase = false;
+      this.showvalidate = false;
+      this.showcompany = true;
+    }
   }
 }
