e40232e392a97e9b905ae4ee9cc84cac460d1c6d
janp110 <303298846@qq.com>
2016-06-16 16:26:40 +0800

    PERF 删除无用组件

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,11 +1,9 @@
 
 import {Component,AfterViewInit,Injectable} from '@angular/core';
-import {ROUTER_DIRECTIVES, RouteConfig} from  '@angular/router-deprecated';
+import {ROUTER_DIRECTIVES, RouteConfig,Router} from  '@angular/router-deprecated';
 import {LoginComponent} from '../+login/index';
 import {SignupComponent} from '../+signup/index';
 import {ForgotComponent} from '../+forgot/index';
-import {Router} from  '@angular/router-deprecated';
-import {MainComponent} from "../+main/index";
 import {ModifyPasswordComponent} from "../+modifypassword/components/modifypwd.component";
 import {FrontPageComponent} from "../+frontpage/index";
 import {DemoComponent} from "../+demo/index";
@@ -85,81 +83,8 @@ import {HomeComponent} from "../+home/home.component";
 ])
 export class AppComponent {
 
-  //isChatShow:boolean = false;//聊天控制
-  //hasMessage:boolean = false;
-  //currentThread:Thread;
-  //newMessageNum:number = 0;
 
   constructor(private router:Router) {
-    //如果没有登陆的话，自动转为登陆页面
-    //if(!sessionStorage.getItem("user")){
-    //  this.router.navigate(['Login']);
-    //}
-    //  ChatExampleData.init(messagesService, threadsService, userService);
-    //  this.threadsService.currentThread.subscribe(
-    //    (thread: Thread) => {
-    //      this.currentThread = thread;
-    //    });
-    //  //计时器
-    //  var self =this;
-    //  self.timer = setInterval(_ => {
-    //    self.hasMessage = self.messagesService.hasNewMessage;
-    //    self.newMessageNum = self.messagesService.newMessageNum;
-    //    if(self.hasMessage){
-    //      $('.ui.Tiny.image')
-    //        .transition('flash')
-    //    }
-    //    //clearInterval(self.timer);
-    //  },1000);
-    //}
 
-    //override
-    //ngAfterViewInit() {
-    //  //jQuery(".item.menu").click(function(){
-    //  //    alert("bb")
-    //  //});
-    //
-    //  jQuery('#talkImage')
-    //    .popup();
-    //
-    //  setTimeout(
-    //    () => {
-    //      this.isChatShow = true;
-    //      this.threadsService.setIsOpenChat(true);
-    //      console.log("ChatShow");
-    //    },
-    //    5000);
-    //
-    //}
-    //chatShow(){
-    //  jQuery('#talkImage')
-    //    .popup('hide');
-    //  this.hasMessage = false;
-    //  this.messagesService.setHasNewMessage(false);
-    //  this.messagesService.setNewMessageNum(this.messagesService.newMessageNum-this.currentThread.newMessageNum);
-    //  this.currentThread.setNewMessageNum(0);
-    //  this.threadsService.setIsOpenChat(true);
-    //  this.isChatShow = true;
-    //
-    //}
-
-    //closeParentWindow(arg){
-    //  this.isChatShow = arg;
-    //  //jQuery('#talkImage')
-    //  //    .popup('reposition');
-    //  jQuery('#talkImage')
-    //    .popup();
-    //  jQuery('#talkImage')
-    //    .popup('show');
-    //
-    //  this.messagesService.setNewMessageNum(this.messagesService.newMessageNum-this.currentThread.newMessageNum);
-    //  if(this.messagesService.newMessageNum == 0){
-    //    this.messagesService.setHasNewMessage(false);
-    //  }else{
-    //    this.messagesService.setHasNewMessage(true);
-    //  }
-    //  this.currentThread.setNewMessageNum(0);
-    //  this.threadsService.setIsOpenChat(false);
-    //}
   }
 }
