5a59c81cf0e7bb7ea97e71b1d3e874f5d0435ab8
janp110 <303298846@qq.com>
2016-06-30 17:33:18 +0800

    FEATURE 新增维护页面

--- src/app/+demo/components/demo.component.ts
+++ src/app/+demo/components/demo.component.ts
@@ -1,22 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import {HighlightDirective} from "../../shared/directives/demo.directive";
 
 @Component({
   selector: 'demo-box',
-  directives: [HighlightDirective],
   styles: [ require('app/+demo/components/demo.component.css') ],
   template: require('app/+demo/components/demo.component.html')
 })
 export class DemoComponent {
-
-
-  constructor() {
-
-  }
-
-
-
+  a: number = 0.255;
+  b: number = 1.3495;
 }
-
 
 
--- src/app/shared/directives/demo.directive.ts
+++ src/app/shared/directives/demo.directive.ts
@@ -3,10 +3,18 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
     selector: '[myHighlight]'
 })
 export class HighlightDirective {
+    private _defaultColor = 'red';
     private el: HTMLElement;
     constructor(el: ElementRef) { this.el = el.nativeElement; }
-    @HostListener('mouseenter') onMouseEnter() {
-        this.highlight('yellow');
+
+    @Input('myHighlight') highlightColor: string;  //也可以直接用@Input() myHighlight: string;
+
+    @Input() set defaultColor(colorName: string){  //defaultColor是一个变量名
+        this._defaultColor = colorName || this._defaultColor;
+    }
+
+    @HostListener('mouseenter') onMouseEnter() {  //父组件触发事件
+        this.highlight(this.highlightColor || this._defaultColor); //@Input注入后，变量直接变为成员变量
     }
     @HostListener('mouseleave') onMouseLeave() {
         this.highlight(null);
